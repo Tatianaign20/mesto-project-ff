9c98140dab4280144bc4fce1fbff60c8fbe72227
Tania Ryabets <tatianaign20@yandex.ru>
2024-05-19 15:39:28 +0300

    mesto_v01

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
-
-// @todo: DOM узлы
+const container = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: Функция создания карточки
 
+function createCard ({name, link}) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true)
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    return cardElement;
+    };
+
 // @todo: Функция удаления карточки
 
+function deleteCard (evt) {
+    const cardDelete = evt.target.closest('.card');
+    cardDelete.remove();
+    };
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(cardElement => {
+    const card = createCard(cardElement);
+    container.append(card);
+    });
+
